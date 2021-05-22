81a0a63fd736d1a674e1b4f2a5e258ed8c3930a1
Prince Uyi <uyimarcel@gmail.com>
2021-05-22 00:51:45 -0400

    corrected a typo error in variable name check rows

--- Prince_5384737_PHP_FinalProject/includes/Validation.php
+++ Prince_5384737_PHP_FinalProject/includes/Validation.php
@@ -1,7 +1,30 @@
 <?php
+// start session // call the function later
     session_start();
 
+    // connect with databse and select the specific db
     $connection = mysqli_connect('localhost', 'root', '');
     mysqli_select_db($connection, 'Classified_Ads_db');
+
+    //verify user inputs
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // query to verify the user input with the user data in the database
+    $vInputs = "SELECT * FROM usertable WHERE email = '$email' && password = '$password' ";
+
+    $result = mysqli_query($connection, $vInputs);
+
+    // check with the rows in usertable
+      $check_rows = mysqli_num_rows($result);
+
+
+      if ($check_rows == 1){
+        $_SESSION['email'] = $email;
+        header('location:home.php');
+    }
+    else  {
+        header('location:login.php');
+    }
 
 ?>
