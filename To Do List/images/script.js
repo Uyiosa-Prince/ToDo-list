1fac526a64d3841dbe103dd0510c1cbaa4c602de
Prince Uyi <uyimarcel@gmail.com>
2021-06-11 21:06:46 -0400

    added the database file

--- To Do List/images/script.js
+++ To Do List/images/script.js
@@ -1,12 +1,12 @@
 function completed() {
     document.getElementById("in-progress").style.display = "none";
     document.getElementById("completed").style.display = "block";
-    // document.getElementById("no-task").style.display = "none";
+    document.getElementById("no-task").style.display = "none";
 }
 
 function inProgress() {
     document.getElementById("in-progress").style.display = "block";
     document.getElementById("completed").style.display = "none";
-    // document.getElementById("no-task").style.display = "none";
+    document.getElementById("no-task").style.display = "none";
 
 }
