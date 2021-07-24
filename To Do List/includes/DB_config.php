9597fe836fe55975c978a2bca001155ac202039a
Prince Uyi <uyimarcel@gmail.com>
2021-07-24 00:48:08 -0400

    added JawsDB in DB_Config file

--- To Do List/includes/DB_config.php
+++ To Do List/includes/DB_config.php
@@ -13,11 +13,17 @@
 // $PASSWORD = 'CIv4VLCXI1';
 // $DBNAME = 'k1PrpRfoLE';
 
-// infinity free mysql web lauch connection
-$HOST = 'sql200.epizy.com';
-$USER = 'epiz_29108012';
-$PASSWORD = '1$Fluteman';
-$DBNAME = 'epiz_29108012_todo_list';
+// // infinity free mysql web lauch connection
+// $HOST = 'sql200.epizy.com';
+// $USER = 'epiz_29108012';
+// $PASSWORD = '1$Fluteman';
+// $DBNAME = 'epiz_29108012_todo_list';
+
+// Heroku JawDB mysql web lauch connection
+$HOST = 's465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$USER = 'firf8pu1k6n6mbbf';
+$PASSWORD = 'gm95l3761tiil3kz';
+$DBNAME = 'nsqyfecx76li0nda';
 
 
 $connection = mysqli_connect($HOST, $USER, $PASSWORD, $DBNAME);
